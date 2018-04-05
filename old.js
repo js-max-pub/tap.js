611c8487aabfb757a516101594fdada7bb517347
Max Fechner <max.fechner@gmail.com>
2018-04-05 11:37:32 +0200

    ...

--- old.js
+++ old.js
@@ -1,3 +1,73 @@
+// TAPJS.TREE2 = node => {
+//     console.log('TREE', node);
+//     var list = [];
+//     while (node) {
+//         list.push(node);
+//         node = node.parentElement;
+//     }
+//     return list;
+// };
+
+// TAPJS.ACT2 = ev => {
+//     console.log(`EVENT-ACT: ${ev.type}`,ev.target,ev);
+//     var path = ev.composedPath ? ev.composedPath() : TAPJS.PATH(ev.target);
+//     console.log('path', path);
+//     var CEs = path.filter(x => (x.tagName && x.tagName.includes('-')));
+//     CEs.push(window);
+//     var CE = CEs[0];
+//     console.log('CEs',CEs);
+
+//     // var CE = ev.target;
+//     ev = JSON.parse(JSON.stringify(ev));
+
+//     for (var i = 0; i < path.length; i++) {
+//         var el = path[i];
+//         if (!el.getAttribute) continue;
+//         // LOG('check '+ el.tagName);
+//         // console.log('check',el);
+//         // if(el.hasAttribute(attr)){
+//         var fn = el.getAttribute('on-tap');
+//         // LOG(`found ${el.tagName}  (${fn})`);
+//         ev.target = el;
+//         if (fn) {
+//             // if (CE[fn]) CE[fn](ev);
+//             // else console.warn(fn, 'not found');
+//             var ces = CEs.filter(ce=>ce[fn]);
+//             if(!ces.length) console.warn(fn,'not found');
+//             ces[0][fn](ev);
+//             // ces.forEach(ce=>ce[fn](ev));
+//         }
+//         // if(CE[fn]) CE[fn](ev);
+//         // else if(window[fn]) window[fn](ev);
+//         // else console.warn(fn,'not found');
+//     }
+// }
+
+
+
+
+
+
+        // console.log('EVENT-FIRE',base);
+
+// LOG('click-delta '+Math.round(window.performance.now() - window.TOUCHEND));
+// this.dispatchEvent( new CustomEvent('tap', {bubbles: true});
+//TAPJS.ACT(ev, 'on-tap');
+
+// this.dispatchEvent( new CustomEvent('tap', {bubbles: true})        
+//TAPJS.ACT(ev, 'on-tap');
+
+
+
+
+    // console.log('TREE', TAPJS.TREE(ev.target));
+    // console.log('composed', ev.composedPath());
+
+    // return;
+    // console.log(ev.composedPath? ev.composedPath():'none');
+    // if (ev.composedPath) var path = ev.composedPath();
+    // else var path = TAPJS.PATH(ev.target);
+
 
 
 // BUBBLEUP = (ev, attr) => {
--- tap.js
+++ tap.js
@@ -1,58 +1,23 @@
 TAPJS = {
-    TREE: node => {
-        console.log('TREE',node);
-        var list = [];
-        while(node){
-            list.push(node);
-            node = node.parentElement;
-        }
-        return list;
-    },
-
-    ACT: (ev,attr) => {
-        // LOG(`EVENT: ${ev.type} (${attr})`);
-        // console.log(ev.composedPath? ev.composedPath():'none');
-        if(ev.composedPath) var path = ev.composedPath();
-        else var path = TAPJS.TREE(ev.target);
-        console.log('path',path);
-        CEs = path.filter(x=>(x.tagName && x.tagName.includes('-')));
-        CEs.push(window);
-        // console.log('CEs',CEs);
-
-        // var CE = ev.target;
-        ev = JSON.parse(JSON.stringify(ev));
-
-        for(var i=0; i<path.length; i++){
-            var el = path[i];
-            if(!el.getAttribute) continue;
-            // LOG('check '+ el.tagName);
-            // console.log('check',el);
-            // if(el.hasAttribute(attr)){
-            var fn = el.getAttribute(attr);
-            // LOG(`found ${el.tagName}  (${fn})`);
-            ev.target = el;
-            if(fn){
-                var ces = CEs.filter(ce=>ce[fn]);
-                ces.forEach(ce=>ce[fn](ev));
-                if(!ces.length) console.warn(fn,'not found');
-            }
-                // if(CE[fn]) CE[fn](ev);
-                // else if(window[fn]) window[fn](ev);
-                // else console.warn(fn,'not found');
-        }
+    event:(ev)=>{
+        var base = ev.composedPath ? ev.composedPath()[0] : ev.target;
+        base.dispatchEvent( new CustomEvent('tap', {bubbles: true,composed: true}));
     }
 }
-
 window.addEventListener('click', ev => {
-    // LOG('click-delta '+Math.round(window.performance.now() - window.TOUCHEND));
-    if(window.performance.now() - TAPJS.TOUCHEND < 500) return ev.preventDefault();
-    TAPJS.ACT(ev, 'on-tap');
+    if(window.performance.now() - TAPJS.TOUCHEND < 500) 
+        return ev.preventDefault();
+    TAPJS.event(ev);
 });
 
 window.addEventListener('touchstart', ev => TAPJS.TOUCHMOVE = false);
 window.addEventListener('touchmove', ev => TAPJS.TOUCHMOVE = true);
 window.addEventListener('touchend', ev => {
     TAPJS.TOUCHEND = window.performance.now();
-    if (!TAPJS.TOUCHMOVE) TAPJS.ACT(ev, 'on-tap');
+    if (!TAPJS.TOUCHMOVE) 
+        TAPJS.event(ev);
 });
+
+
+
 
